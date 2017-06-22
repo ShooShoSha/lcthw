0fcdde8210b6ba2a789ff02a2cd844e318cceae3
ShooShoSha <me@shooshosha.com>
2017-06-22 11:57:04 -0700

    Finish drills.

--- exercises/ex3.py
+++ exercises/ex3.py
@@ -1,22 +1,31 @@
+# Prints a statement.
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+# Prints the string and result of expression.
+print("Hens", 25.0+ 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 
+# Prints a statement.
 print("Now I will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 %2 - 1 / 4 + 6)
+# Prints the result of the expression.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
-print("Is it true that 3 + 2 < 5 - 7?")
-print(3 + 2 < 5- 7)
+# Prints a statement.
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+# Prints the result of the expression.
+print(3.0 + 2.0 < 5.0 - 7.0)
 
+# Prints the string and result of expression.
+print("What is 3.0 + 2.0?", 3.0 + 2.0)
+print("What is 5.0 - 7.0?", 5.0 - 7.0)
+
+# Prints a statement.
 print("Oh, that's why it's False.")
-
 print("How about some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+# Prints the string and result of expression.
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
---
+++ exercises/ex3d1.py
@@ -0,0 +1,6 @@
+# Values are fictious that serve as placeholders.
+print("Income (monthly):", 429 * 4)
+print("Expenses (monthly):", 1100 + 70 + 70 + 131.33 + 80 + 19 + 15)
+
+# Decimal is not accurate, displays "230.67000000000007"
+print("Unbudgeted:", 1716.0 - 1485.33)
